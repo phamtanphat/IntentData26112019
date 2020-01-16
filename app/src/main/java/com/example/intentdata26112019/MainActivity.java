ef98c5f7619e613d1a69bdd76c10d37758863349
phatdroid94 <phatdroid94@gmail.com>
2020-01-16 18:38:50 +0700

    generic

--- app/src/main/java/com/example/intentdata26112019/MainActivity.java
+++ app/src/main/java/com/example/intentdata26112019/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     Button mBtnIntentString,mBtnIntentArray;
@@ -30,10 +33,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int [] arrayNumbers = {1,2,3,4,5,6,7,8,9,10};
-                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
-                intent.putExtra("mang so",arrayNumbers);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+//                intent.putExtra("mang so",arrayNumbers);
+//                startActivity(intent);
+                sendData("mang so",arrayNumbers);
             }
         });
+    }
+    private <T> void  sendData(String key,T value){
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        if (value instanceof String){
+            intent.putExtra(key,(String) value);
+        }
+        if (value instanceof int[]){
+            intent.putExtra(key,(int[]) value);
+        }
+        startActivity(intent);
     }
 }
